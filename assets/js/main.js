4fbecd47854da33abfaefc98efbe92cdf4a75b5b
mativiscusso <mativiscusso@gmail.com>
2021-06-23 16:29:10 -0300

    last changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+function stickyElement(e) {
+    var navbar = document.querySelector(".menu");
+    var logoMenu = document.getElementById("logo-menu");
+    var scrollValue = window.scrollY;
+
+    if (scrollValue > 100) {
+        navbar.classList.add("is-fixed");
+        logoMenu.classList.add("fixed");
+    } else if (scrollValue < 50) {
+        navbar.classList.remove("is-fixed");
+        logoMenu.classList.remove("fixed");
+    }
+}
+
+window.addEventListener("scroll", stickyElement);
+
 document.addEventListener("scroll", () => {
     const backToTop = document.querySelector(".back-to-top-wrapper");
     if (window.scrollY > 500) {
